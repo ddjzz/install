2e99fecfd87cc745c377d6eb0661f09a074dde41
atwood <atwood@localhost>
2013-09-10 15:52:19 +0800

    Add samba install and config file.

---
+++ samba/install.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash -e
+
+# samba : service proveder
+# system-config-samba : samba config tool
+# python-glade2 : inorder to install and use system-config-samba
+
+sudo apt-get install -y samba samba-common python-glade2 system-config-samba
+
