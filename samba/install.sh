0fb87cf2e785c195ac97063af972eb8fca7c2093
atwood <atwood@localhost>
2013-09-13 17:45:03 +0800

    samba : fix apt-get error by rm -y option.

--- samba/install.sh
+++ samba/install.sh
@@ -5,5 +5,5 @@
 # system-config-samba : samba config tool
 # python-glade2 : inorder to install and use system-config-samba
 
-sudo apt-get install -y samba samba-common python-glade2 system-config-samba
+sudo apt-get install  samba samba-common python-glade2 system-config-samba
 
